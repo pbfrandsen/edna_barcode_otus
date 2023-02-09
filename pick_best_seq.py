d8fec95e45e38fd21081a08d4aee9b94b0302225
Paul Frandsen <paulbfrandsen@gmail.com>
2023-02-08 20:14:00 -0700

    removed print statements

--- pick_best_seq.py
+++ pick_best_seq.py
@@ -24,7 +24,7 @@ with open(infilename) as infile:
         else:
             if new_line[0] == qseq:
                 if float(new_line[10]) < float(evalue):
-                    print("ha")
+                    # print("ha")
                     taxid = new_line[12]
                     if ";" in taxid:
                         taxid = taxid.split(";")[0] + "\n"
@@ -49,7 +49,7 @@ with open(infilename) as infile:
                 perc_id = new_line[2]
         best_seqs[qseq] = [taxid, perc_id]
 
-print(best_seqs)
+# print(best_seqs)
 with open(outfilename, "w") as outfile:
     for record in best_seqs:
         outfile.write(record + "\t" + best_seqs[record][0].strip() + "\t" + best_seqs[record][1] + "\n")
